e9ef2de7d54189edfd2f7f26c005c36dee341d22
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-06-22 18:42:41 -0300

    scriptMenu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,18 @@
+//evento de clique para alternar a exibição do menu dropdown
+
+var dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(function(dropdown) {
+  var dropdownMenu = dropdown.querySelector('.dropdown-menu');
+  var dropdownToggle = dropdown.querySelector('a');
+
+  dropdownToggle.addEventListener('click', function(event) {
+    event.preventDefault();
+    dropdownMenu.classList.toggle('show');
+  });
+});
+
+//fechar o menu dropdown ao clicar fora dele
 document.addEventListener('click', function(event) {
     var isClickInside = false;
     dropdowns.forEach(function(dropdown) {
